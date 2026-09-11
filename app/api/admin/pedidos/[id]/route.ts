f30854a8e11115ce6c25ff7c5785444ae4a37842
jhonathancruz2007 <jhonathancruz2007@gmail.com>
2026-09-11 16:50:29 -0300

    Update route.ts

--- app/api/admin/pedidos/[id]/route.ts
+++ app/api/admin/pedidos/[id]/route.ts
@@ -5,7 +5,7 @@ import nodemailer from 'nodemailer'
 // Configuração do transportador de e-mail usando contato@jkfashionkids.com.br
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST || 'smtp.gmail.com',
-  port: Number(process.env.EMAIL_PORT) || 587,
+  port: Number(process.env.EMAIL_PORT) || 465,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER, // Variável com o e-mail (ou senha específica de app)
